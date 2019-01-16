5d932e3bfe0e5f02c9a20bd8a90076850a02a2c1
travis mills <travismills82@gmail.com>
2019-01-15 18:44:08 -0800

    add vendor/media firmware

--- universal8895-common-vendor-blobs.mk
+++ universal8895-common-vendor-blobs.mk
@@ -225,4 +225,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/universal8895-common/proprietary/vendor/etc/wifi/wldu.conf:system/vendor/etc/wifi/wldu.conf \
     vendor/samsung/universal8895-common/proprietary/vendor/etc/wifi/.clminfo:system/vendor/etc/wifi/.clminfo \
     vendor/samsung/universal8895-common/proprietary/lib/vendor.samsung.hardware.radio@1.0.so:system/lib/vendor.samsung.hardware.radio@1.0.so \
-    vendor/samsung/universal8895-common/proprietary/lib64/vendor.samsung.hardware.radio@1.0.so:system/lib64/vendor.samsung.hardware.radio@1.0.so
+    vendor/samsung/universal8895-common/proprietary/lib64/vendor.samsung.hardware.radio@1.0.so:system/lib64/vendor.samsung.hardware.radio@1.0.so \
+    vendor/samsung/universal8895-common/proprietary/vendor/media/LMspeed_508.emd:system/vendor/media/LMspeed_508.emd \
+    vendor/samsung/universal8895-common/proprietary/vendor/media/PFFprec_600.emd:system/vendor/media/PFFprec_600.emd
